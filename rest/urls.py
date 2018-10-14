570b3d8e413c941c7604f28ad28475cd1f70d3d0
lautarovc <casper1994@gmail.com>
2018-10-14 19:42:15 -0400

    Inicia rest app

--- rest/urls.py
+++ rest/urls.py
@@ -1,7 +1,7 @@
-"""buscaMedServer URL Configuration
+"""rest URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/2.1/topics/http/urls/
+    https://docs.djangoproject.com/en/2.0/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
@@ -13,9 +13,9 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import include, path
+from django.urls import path, include
+from . import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-]
+    path('', views.home_view, name='home')
+    ]
