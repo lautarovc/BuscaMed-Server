90c9c809eb51375ebdaef8660a6aacb367a19234
lautarovc <casper1994@gmail.com>
2018-10-14 16:52:28 -0400

    Create Django

---
+++ env/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/mnt/01D2566A6446CF50/Users/Owner/Documents/USB/MiniProyecto/BuscaMed Server/env/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
