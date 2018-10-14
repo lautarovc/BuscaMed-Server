705c9283e9ab722c8e4452b4bd40f97faac5715a
lautarovc <casper1994@gmail.com>
2018-10-14 17:08:39 -0400

    Borra virtualenv

--- env/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/mnt/01D2566A6446CF50/Users/Owner/Documents/USB/MiniProyecto/BuscaMed Server/env/bin/python3
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
