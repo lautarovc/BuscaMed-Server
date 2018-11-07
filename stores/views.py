64d36187fbcaa536cdf395b71b7d0cd1960f4fd9
yariluciani <yari.luciani95@gmail.com>
2018-11-06 22:07:07 -0400

    Agrega actualizacion BD del archivo csv

--- stores/views.py
+++ stores/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
+from .models import *
 import json 
 
 # Create your views here.
@@ -17,7 +18,31 @@ class StoresView(TemplateView):
 
 		data = json.loads(data)
 
-		
+		productosViejos = ProductosPorTienda.objects.filter(tienda=request.user).delete()
 
+		for row in data:
+			componenteMed = row['activo']
+			activo = Activo.objects.filter(componente=componenteMed)
+
+			if activo.count() == 0:
+				activo = Activo.objects.create(componente=componenteMed)
+
+
+			nombreMed = row['medicina']
+			medicina = Medicina.objects.filter(nombre=nombreMed)
+
+			if medicina.count() == 0:
+				medicina = Medicina.objects.create(nombre=nombreMed, activo=activo)
+
+
+			presentacionMed = row['presentacion']
+			presentacion = Presentacion.objects.filter(presentacion=presentacionMed, medicina=medicina)
+
+			if presentacion.count() == 0:
+				presentacion = Presentacion.objects.create(presentacion=presentacionMed, medicina=medicina)
+
+
+			disponibilidadMed = row['disponibilidad']
+			productosPorTienda = ProductosPorTienda.objects.create(medicina=presentacion, tienda=request.user, disponibilidad=disponibilidadMed) 
 
 		return render(request, 'readFile.html', {})
